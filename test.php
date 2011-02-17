6b911ea5c0cfe6953097fa2b0bbdae87722fb914
Michael Wheeler <mwheeler@carbon.mammoth.nsw.au.fooproject.net>
2011-02-17 18:50:34 +1000

    Updated to make more inline with current setup

--- test.php
+++ test.php
@@ -12,40 +12,22 @@ $subnetmask = 24; /* subnet mask  inverted*/
 
 
 
-
-
-
-
-/* http://forums.devnetwork.net/viewtopic.php?f=1&t=114832&sid=485e918e5322f81877c585d701f9b9d1&start=15 */
-
-function isValidIPv4Mask($mask)
-{
-        if ($mask == (int) 0xffffffff || !$mask) return false;
-
-        while (!($mask & 1))
-                $mask >>= 1;
-
-        return $mask == (int) 0xffffffff;
-}
-
-
-
 function cidr2mask($cidr)
 {
-if (($cidr > 0) & ($cidr <= 32)){
-$mask = 4294967296 - pow(2, 32-$cidr);
-return $mask;
-} else {
-return false;
-}
+	if (($cidr > 0) & ($cidr <= 32)){
+		$mask = 4294967296 - pow(2, 32-$cidr);
+		return $mask;
+	} else {
+		return false;
+	}
 }
 
 
 
 
 function printbin($value){
-$format = '%0' . (PHP_INT_SIZE * 8) . "b\n"; 
-printf($format, $value);
+	$format = '%0' . (PHP_INT_SIZE * 8) . "b\n"; 
+	printf($format, $value);
 }
 
 /*
@@ -54,25 +36,27 @@ stop when the subnet is larger than the network + the networkmask ,
 increment by the subnetmask (+1 to move it into the subnet area)
 */
 function findnetworks($network, $networkmask, $subnetmask){
-	$network = $network& ~ $networkmask; /* make sure the network is infact a network address by xor'ing the network mask */
-	$networkmask = ~ cidr2mask($networkmask); // inverted net mask
-	$subnetmask = ~ cidr2mask($subnetmask); // inverted subnet mask
-	if ($networkmask & $subnetmask){
-		for ( $subnet = $network; $subnet < $network ^ $networkmask ^ $subnetmask ; $subnet += $subnetmask + 1) { 
-			if( $subnet > ($network ^ $networkmask ^ $subnetmask)){
-				break;
-			} 
-			printsubnet($subnet);
+	if ($networkmask > $subnetmask) return ("CIDR validation failed - Netmask larger than subnetmask\n");
+	$networkmask = cidr2mask($networkmask); // net mask checks and conversion
+	$subnetmask = cidr2mask($subnetmask); //  subnet mask checks and conversion
+	if (!($networkmask & $subnetmask)) return ("CIDR validation failed - to big or to small\n");
+	$network = $network & $networkmask; /* make sure the network is infact a network address by xor'ing the network mask */
+	$networkmask = ~ $networkmask ; // invert masks
+	$subnetmask = ~ $subnetmask ;  //invert masks
+	for ( $subnet = $network; $subnet < $network ^ $networkmask ^ $subnetmask ; $subnet += $subnetmask + 1) { 
+		if( $subnet > ($network ^ $networkmask ^ $subnetmask)){
+			break;
 		} 
-	} else {
-		return ("CIDR validation failed");
-	}
+		printsubnet($subnet);
+	} 
 }
 
 function printsubnet($subnet){
-print long2ip($subnet);
-print "\n";
+	print long2ip($subnet); /* for now just print out the ip address */
+	print "\n";
 }
 
 print findnetworks($network, $networkmask, $subnetmask);
+print findnetworks($network, 32, 32);
+
 ?>
