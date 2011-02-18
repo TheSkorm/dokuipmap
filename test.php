f96b4c33b9f258abbc32efee02efe18c42dd0b34
Michael Wheeler <mwheeler@carbon.mammoth.nsw.au.fooproject.net>
2011-02-18 17:08:42 +1000

    Win? added in finding combined subnets

--- test.php
+++ test.php
@@ -8,6 +8,12 @@
 $network = ip2long("172.16.0.0"); /* this is the network address as a number */
 $networkmask = 16; /* network mask inverted */
 $subnetmask = 24; /* subnet mask  inverted*/
+$KnownNetworks  = array();
+// all network keys should be checked using an XOR of the mask first.
+$KnownNetworks[ip2long("172.16.7.0")] = cidr2mask(24);
+$KnownNetworks[ip2long("172.16.16.0")] = cidr2mask(23);
+print_r($KnownNetworks);
+print_r(array_keys($KnownNetworks));
 
 
 
@@ -30,18 +36,13 @@ function printbin($value){
 }
 
 
-$KnownNetworks  = array();
-// all network keys should be checked using an XOR of the mask first.
-$KnownNetworks[ip2long("172.16.7.0")] = cidr2mask(24);
-$KnownNetworks[ip2long("172.16.16.0")] = cidr2mask(23);
-print_r($KnownNetworks);
-print_r(array_keys($KnownNetworks));
 /*
 start at the network address,
 stop when the subnet is larger than the network + the networkmask ,
 increment by the subnetmask (+1 to move it into the subnet area)
 */
-function findnetworks($network, $networkmask, $subnetmask, $knownnetworks){
+function findnetworks($network, $networkmask, $subnetmask){
+	$subnets = array();
 	if ($networkmask > $subnetmask) return ("CIDR validation failed - Netmask larger than subnetmask\n");
 	$networkmask = cidr2mask($networkmask); // net mask checks and conversion
 	$subnetmask = cidr2mask($subnetmask); //  subnet mask checks and conversion
@@ -50,20 +51,37 @@ function findnetworks($network, $networkmask, $subnetmask, $knownnetworks){
 	$networkmask = ~ $networkmask ; // invert masks
 	$subnetmask = ~ $subnetmask ;  //invert masks
 	for ( $subnet = $network; $subnet <= ($network ^ $networkmask ^ $subnetmask ); $subnet += $subnetmask + 1) { 
-		printsubnet($subnet, $knownnetworks);
+		$subnets[] = $subnet;
 	} 
+	return $subnets;
 }
 
-function printsubnet($subnet,$knownnetworks){
-	if(in_array($subnet, array_keys($knownnetworks))){
-	print "found ya\n";
-	print long2ip($knownnetworks[$subnet]);
-	print "\n";
+function printsubnet($subnets,$knownnetworks){
+	$lastmatch = array(0 => 0,1 => 0); //0=ip address, 1=subnetmask
+	foreach ($subnets as $subnet){ //run through each subnet
+		if ($lastmatch[0] or $lastmatch[1]){ //Did we spot a known subnet on the last loop?
+			if (($subnet & $lastmatch[1])==$lastmatch[0]){ // check if the subnet fits within the last spotted subnet and subnet mask
+				print long2ip($lastmatch[0]);
+				print "* \n";
+			} else { // if it's not, it means we have gone passed out subnet mask and need to reset
+				$lastmatch = array(0 => 0,1 => 0);
+                                print long2ip($subnet);
+                                print "\n";
+			}
+		}  else {
+			if(in_array($subnet, array_keys($knownnetworks))){  //is the subnet known?
+				print long2ip($subnet);
+				print "* \n";
+				$lastmatch[0] = $subnet;
+				$lastmatch[1] = $knownnetworks[$subnet];
+			} else {						//not known subnet
+                                print long2ip($subnet);
+                                print "\n";
+
+			}
+		}
 	}
-	print long2ip($subnet); /* for now just print out the ip address */
-	print "\n";
 }
-
-print findnetworks($network, $networkmask, $subnetmask,$KnownNetworks);
+print printsubnet(findnetworks($network, $networkmask, $subnetmask),$KnownNetworks);
 
 ?>
